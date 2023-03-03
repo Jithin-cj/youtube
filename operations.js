bfda539bc98ddd17e7d1f7c255dd655b1e72bff0
Jithin-cj <54771544+Jithin-cj@users.noreply.github.com>
2023-03-03 23:51:10 +0530

    Update operations.js

--- operations.js
+++ operations.js
@@ -1,7 +1,7 @@
 
 
 function clickHandler() {
-    var url ="https://www.youtube.com/embed/"+document.getElementById("inputVal").value
+    var url =document.getElementById("inputVal").value.replace("watch?v=", "embed/");
 //EAR7De6Goz4
     document.getElementById("demo").setAttribute("src", url);
     console.log(url)
