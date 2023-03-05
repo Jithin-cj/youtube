b423ca6fc3b9b06fabf1a7540ac0e4d06baa0c65
Jithin-cj <54771544+Jithin-cj@users.noreply.github.com>
2023-03-06 01:23:44 +0530

    Update operations.js

--- operations.js
+++ operations.js
@@ -1,8 +1,16 @@
 
 
+let intialCode = window.location.search.replace("?code=", "");
+console.log("intial url" + intialCode);
+document.getElementById("demo").setAttribute("src","https://www.youtube.com/embed/"+ intialCode);
+
 function clickHandler() {
-    var url =document.getElementById("inputVal").value.replace("watch?v=", "embed/");
+    var code =document.getElementById("inputVal").value.split("&")[0].replace("https://www.youtube.com/watch?v=", "");
 //EAR7De6Goz4
-    document.getElementById("demo").setAttribute("src", url);
-    console.log(url)
+    //document.getElementById("demo").setAttribute("src", url);
+    window.location.href = window.location.href.split("?")[0]+"?code="+code;
+
+    console.log(code)
 }
+
+//https://www.youtube.com/watch?v=OPaLnMw2i_0&list=PL6QREj8te1P7gixBDSU8JLvQndTEEX3c3
